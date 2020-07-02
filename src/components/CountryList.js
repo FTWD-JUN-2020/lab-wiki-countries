2515020334e6df9e84b2c8c609bd8262dd7c384b
ajhdrummer <thedrew88@gmail.com>
2020-07-02 15:35:57 -0400

    d

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import countries from "../countries.json"
+
 
 const CountryList = (props) => {
+	
     return (
-        <li><Link to='/'>{props.name}</Link></li>
+        <li>
+        <Link to={props.code}>{props.name}</Link>
+        </li>
     )
 }
 export default CountryList
